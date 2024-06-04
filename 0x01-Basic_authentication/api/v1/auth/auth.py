8ededdf6e2002057d819293e85a061fd76ac8682
Ailwei <Ailweimaemu@gmail.com>
2024-06-04 14:04:12 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
+""" Module of Authentication
 """
-you will create a class to manage the API authentication
-"""
-
-
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """ Class to manage API authentication """
+    """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method for validating if endpoint requires auth """
         if path is None:
             return True
-        if excluded_paths is None or exclude_paths == []:
+        if excluded_paths is None or excluded_paths == []:
             return True
-        if path.endswith('/'):
-            path = path[:-1]
 
-        for exec_path in excluded_paths:
-            if ecex_path.endswith('/'):
-                exec_path = exec_path[:-1]
-            if path == exec_path:
+        if path.endswith('/'):
+            path = path[:-1]  # Remove trailing slash to match excluded_paths
+
+        for exc_path in excluded_paths:
+            if exc_path.endswith('/'):
+                exc_path = exc_path[:-1]  # Remove trailing slash to match path
+            if path == exc_path:
+                return False
+            if exc_path.endswith('*') and path.startswith(exc_path[:-1]):
                 return False
 
         return True
 
     def authorization_header(self, request=None) -> str:
         """ Method that handles authorization header """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Validates current user """
