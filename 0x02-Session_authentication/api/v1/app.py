499234fc922169db0750ed371013aedf99530e4a
Ailwei <Ailweimaemu@gmail.com>
2024-06-05 22:50:48 +0000

    update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,51 +5,34 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
 
-if AUTH_TYPE == "auth":
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+
+# Check the AUTH_TYPE
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorizzed_error(error):
-    """
-    Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_error(error):
-    """
-    Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
 def before_request():
-    """
-     Before Request Handler
+    """_summary_
+
+    Returns:
+        _type_: _description_
     """
     if auth is None:
         pass
@@ -65,6 +48,39 @@ def before_request():
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """_summary_
+
+    Args:
+        error (_type_): _description_
+
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """_summary_
+
+    Args:
+        error (_type_): _description_
+
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
