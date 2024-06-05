0d4488c73523b5d1cae9bd50c020a8d141b7800b
Ailwei <Ailweimaemu@gmail.com>
2024-06-05 22:42:16 +0000

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,22 +52,19 @@ def before_request():
      Before Request Handler
     """
     if auth is None:
-        return
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded_list = ['/api/v1/status/',
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
-    excluded_paths = [
-            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
-            ]
-    if request.path in excluded_paths:
-        return
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
+        if auth.require_auth(request.path, excluded_list):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
 
 
 if __name__ == "__main__":
