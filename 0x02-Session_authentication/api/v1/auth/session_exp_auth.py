d219e9178814838d40f621f379765eca987e5a43
Ailwei <Ailweimaemu@gmail.com>
2024-06-06 13:45:32 +0000

    update session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -24,13 +24,13 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        
+
         session_dict = {
             "user_id": user_id,
             "created_at": datetime.now()
         }
         self.user_id_by_session_id[session_id] = session_dict
-        
+
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
@@ -47,11 +47,14 @@ class SessionExpAuth(SessionAuth):
 
         if self.session_duration <= 0:
             return user_id
-        
+
         if created_at is None:
             return None
 
-        if created_at + timedelta(seconds=self.session_duration) < datetime.now():
+        if created_at + timedelta(
+                seconds=self.session_duration
+                )
+        < datetime.now():
             return None
 
         return user_id
